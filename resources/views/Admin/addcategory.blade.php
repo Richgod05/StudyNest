f85647ecc139b88845fa8760981ef0d51a624cc2
Richgod05 <richgodpallangyo@gmail.com>
2025-10-14 15:55:20 +0300

    updated add category

--- resources/views/Admin/addcategory.blade.php
+++ resources/views/Admin/addcategory.blade.php
@@ -1,1 +1,7 @@
 
+<h2>Add New Category</h2>
+<form method="POST" action="{{ route('admin.storeCategory') }}">
+    @csrf
+    <input type="text" name="name" placeholder="Category name" required>
+    <button type="submit">Add Category</button>
+</form>
