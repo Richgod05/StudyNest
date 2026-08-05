8e8d48416162f2eb2f9e540fa55becf541907cc0
Richgod05 <richgodpallangyo@gmail.com>
2026-08-05 15:55:06 +0300

    Creating admin redirect

---
+++ app/Http/Middleware/AdminRedirect.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminRedirect
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // If already logged in as admin, send to dashboard
+        if (Auth::guard('admins')->check()) {
+            return redirect()->route('admin.home');
+        }
+
+        // Otherwise proceed to login form or authenticate
+        return $next($request);
+    }
+}
