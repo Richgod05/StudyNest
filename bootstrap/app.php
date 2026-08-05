30dc4c9d326142c14702e6023bb3ba9b7c80bcc1
Richgod05 <richgodpallangyo@gmail.com>
2026-08-05 16:50:38 +0300

    providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin.guest' => \App\Http\Middleware\AdminRedirect::class,
+            'admin.auth'  => \App\Http\Middleware\AdminAuthenticate::class,
+        ]); 
+
+
+        $middleware -> redirectTo(
+            guests: '/login',
+            users: '/logout'
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
