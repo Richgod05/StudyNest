897333fc7e74fb3d34811482779ce5215df8a0d6
Richgod05 <richgodpallangyo@gmail.com>
2026-08-05 15:49:28 +0300

    Admin login page and middleware

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminAuthenticate
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::guard('admins')->check()){
+            return redirect()->route('admin.adminlogin');
+        }
+
+        return $next($request);
+        
+    }
+    
+}
